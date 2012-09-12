a0779f01af88f9fa4c5444419956ed872db9f578
Wayne Denier <wayne.denier@gmail.com>
2012-09-11 21:43:45 -0700

    Created test for Fizz condition

--- FizzBuzz/FizzBuzz.Test/FizzBuzzRunnerTest.cs
+++ FizzBuzz/FizzBuzz.Test/FizzBuzzRunnerTest.cs
@@ -13,32 +13,54 @@ namespace FizzBuzz.Test
     public class FizzBuzzRunnerTest
     {
         private readonly MockRepository _mockRepository = new MockRepository();
+        private FizzBuzzRunner _runner;
+        private MockConsoleWriter _mockConsoleWriter;
+
+        [TestInitialize]
+        public void Init()
+        {
+            // create runner and writer
+            _runner = new FizzBuzzRunner();
+
+            // set up writer to track number of lines writen
+            _mockConsoleWriter = new MockConsoleWriter();
+            _runner.Writer = () => _mockConsoleWriter;
+        }
 
         [TestMethod]
         public void When_Run_Is_Called_Should_Write_100_Lines()
         {
-            // create runner and writer
-            var runner = new FizzBuzzRunner();
-
-            // set up writer to track number of lines writen
-            var mockConsoleWriter = new MockConsoleWriter();
-            runner.Writer = () => mockConsoleWriter;
-
             // run fizz buzz
-            runner.Run();
+            _runner.Run();
 
             // check lines
-            mockConsoleWriter.LinesWriten.ShouldEqual(100);
+            _mockConsoleWriter.LinesWriten.ShouldEqual(100);
         }
+
+        [TestMethod]
+        public void When_Run_Is_Called_Values_Divisable_By_3_Should_Print_Fizz()
+        {
+            // run fizz buzz
+            _runner.Run();
+
+            // check lines
+            Assert.IsTrue(_mockConsoleWriter.Lines.Count >= 9);
+            _mockConsoleWriter.Lines[2].ShouldEqual("Fizz");
+            _mockConsoleWriter.Lines[5].ShouldEqual("Fizz");
+            _mockConsoleWriter.Lines[8].ShouldEqual("Fizz");
+        }
+
     }
 
     public class MockConsoleWriter : IConsoleWriter
     {
         public int LinesWriten = 0;
+        public List<string> Lines = new List<string>();
 
         public void WriteLine(string line)
         {
             LinesWriten++;
+            Lines.Add(line);
         }
     }
 }
